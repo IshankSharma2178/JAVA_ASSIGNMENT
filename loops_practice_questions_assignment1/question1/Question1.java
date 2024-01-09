2dcdbd18e055b1accf64e6b8b865d0b7fe74160f
IshankSharmaGithub <ishank.sharma.github@gmail.com>
2024-01-09 20:49:00 +0530

    change the logic to for loop in question1

--- loops_practice_questions_assignment1/question1/Question1.java
+++ loops_practice_questions_assignment1/question1/Question1.java
@@ -1,6 +1,10 @@
 package question1;
 class Question1 {
     public static void main(String arg[]){
+
+        for (int i = 0; i <10; i++) {
+            System.out.println(i);
+        }
         System.out.print("aa");
     }
 }
